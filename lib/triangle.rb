e474c9af30c555b4398b625a7f2a7a42eaeeb6b3
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-10-26 00:50:35 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+def kind
+  validate_triangle
+
+  if side1==side2 && side2==side3
+    :equilateral
+  elsif side1!=side2 && side2!=side2 && side3!=side1
+    :scalene
+  else
+    :isoslece
+  end
+
 end
+
